18d2a90a332236279d6be9abbfc5bd8dd93e5449
curder <q.curder@gmail.com>
2021-03-17 16:30:14 +0800

    simple component example instead @section and @extends

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel blade component demo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.3/tailwind.min.css">
+</head>
+<body>
+    {{ $slot }}
+</body>
+</html>
---
+++ resources/views/components/section.blade.php
@@ -0,0 +1,5 @@
+<section class="bg-blue-200 p-4 my-6">
+    <div class="container">
+        {{ $slot }}
+    </div>
+</section>
